ed3a26077e23d0dbae2fb6381f358b8efddad977
Steven Steiner <steets250@gmail.com>
2021-02-16 14:34:52 -0800

    Add Components and Starter HTML

---
+++ source/components/pomo-finish.js
@@ -0,0 +1,15 @@
+class PomoFinish extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({mode: 'open'});
+
+        const element = document.createElement('p');
+
+        element.textContent = "Finish";
+        
+        shadow.appendChild(element);
+      }
+}
+
+customElements.define('pomo-finish', PomoFinish);
