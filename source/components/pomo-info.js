7548c82b91e44e82646ac690d1bbd394cfc7e1f3
Steven Steiner <steets250@gmail.com>
2021-02-16 14:34:52 -0800

    Add Components and Starter HTML

---
+++ source/components/pomo-info.js
@@ -0,0 +1,15 @@
+class PomoInfo extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({mode: 'open'});
+
+        const element = document.createElement('p');
+
+        element.textContent = "Info";
+        
+        shadow.appendChild(element);
+      }
+}
+
+customElements.define('pomo-info', PomoInfo);
---
+++ source/components/pomo-timer.js
@@ -0,0 +1,15 @@
+class PomoTimer extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({mode: 'open'});
+
+        const element = document.createElement('p');
+
+        element.textContent = "Timer";
+        
+        shadow.appendChild(element);
+      }
+}
+
+customElements.define('pomo-timer', PomoTimer);
