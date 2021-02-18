cfee9a61ba0950500b2eb6df066eb6bd99dfd7c3
ntrappe <ntrappe@ucsd.edu>
2021-02-17 17:30:24 -0800

    timer working, helper functions now in separate file, browser tab updates too

---
+++ source/components/pomo-timer-helpers.js
@@ -0,0 +1,84 @@
+/* Helper Functions for Timer (imported by pomo-timer.js) */
+
+const MAX_SEC = 60;
+const NUM_MIN = 2;
+const SEC_SPEED = 250;
+const TWO_DIGIT = 10;
+const START = 'Start';
+const RESET = 'Reset';
+
+/**
+ * Uses the total seconds to split into minutes and seconds and display on screen
+ * @param {Number} totalSeconds current total number of seconds on timer
+ * @param {String} timerText text for current time on timer
+ */
+function display(totalSeconds, timerText) {
+    // calculate minutes and seconds from total seconds in timer
+    let minutes = Math.floor(totalSeconds / MAX_SEC);
+    let seconds = totalSeconds - (minutes * MAX_SEC);
+
+    let t_min;              // text repr of minutes 
+    let t_sec;              // text repr of seconds
+
+    // pad with zeros if necessary
+    if (minutes < TWO_DIGIT) {
+        t_min = '0' + String(minutes);
+    } else {
+        t_min = String(minutes);
+    }
+
+    // pad with zeros if necessary
+    if (seconds < TWO_DIGIT) {
+        t_sec = '0' + String(seconds);
+    } else {
+        t_sec = String(seconds);
+    }
+
+    // write text to screen
+    timerText.textContent = t_min + ':' + t_sec;
+    setTab(t_min, t_sec);
+}
+
+/**
+ * Updates browser tab text and icon to reflect time
+ * @param {String} t_min text version of curr minute
+ * @param {String} t_sec text version of curr second
+ */
+function setTab(t_min, t_sec) {
+    let tabText = document.querySelector('title');
+    tabText.textContent = 'Pomo: ' + t_min + ':' + t_sec;
+}
+
+/**
+ * Depending on the minutes given, will set the total seconds of timer appropriately 
+ * @param {Number} m Minutes for the timer to run
+ * @return {Number} totalSeconds
+ */
+function set(m) {
+    // Error catching: make sure minimum 1m and maximum 59m
+    if (m < 1 || m > 59) {
+        return(NUM_MIN * MAX_SEC);
+    } else {
+        return(m * MAX_SEC);
+    }
+}
+
+/**
+ * Changes button to represent Start
+ *  @param {Button} timerButton button for Start/Reset timer
+ */
+function setStartButton(timerButton) {
+    timerButton.innerHTML = START;
+}
+
+/**
+ * Changes button to represent Reset
+ *  @param {Button} timerButton button for Start/Reset timer
+ */
+function setResetButton(timerButton) {
+    timerButton.innerHTML = RESET;
+}
+
+export {display, setStartButton, setResetButton, set};
+
+/* End pomo-timer-helpers.js */
--- source/components/pomo-timer.js
+++ source/components/pomo-timer.js
@@ -1,3 +1,10 @@
+import { setStartButton, setResetButton, display, set } from './pomo-timer-helpers.js';
+
+const START = 'Start';
+const RESET = 'Reset';
+const NUM_MIN = 2;
+const SEC_SPEED = 250;
+
 class PomoTimer extends HTMLElement {
     constructor() {
         super();
@@ -5,11 +12,76 @@ class PomoTimer extends HTMLElement {
         const shadow = this.attachShadow({mode: 'open'});
 
         const element = document.createElement('p');
-
         element.textContent = "Timer";
+
+        let timerText = document.createElement('h1');
+        timerText.textContent = "02:00";
         
+        let timerButton = document.createElement('button');
+        timerButton.textContent = START;
+
+        let totalSeconds = 0;
+        let ticker;
+
         shadow.appendChild(element);
-      }
+        element.appendChild(timerText);
+        element.appendChild(timerButton);
+
+        /**
+         * Function calls to toggle button and control timer based on user input
+         */
+        timerButton.onclick = () => {
+            if (timerButton.textContent === START) {
+                setResetButton(timerButton);
+                timerStart();
+            } else {
+                setStartButton(timerButton);
+                timerReset();
+            }
+        };
+
+        /**
+        * Stops the timer from ticking and resets it based on button click
+        */
+        function timerReset() {
+            clearInterval(ticker);
+            totalSeconds = set(NUM_MIN);
+            display(totalSeconds, timerText);
+        }
+
+        /**
+         * Sets time on timer and starts ticking based on button click
+         */
+        function timerStart() {
+            totalSeconds = set(NUM_MIN);
+            display(totalSeconds, timerText);
+            ticker = setInterval(tick, SEC_SPEED);
+        }
+
+        /**
+        * Timer hits 0:00 and resets
+        */
+        function timerFinish() {
+            clearInterval(ticker);
+            totalSeconds = set(NUM_MIN);
+            display(totalSeconds, timerText);
+        }
+
+        /**
+        * Decrement each second. If time has run out, stop "ticking" by clearing Interval and 
+        * reset the timer. Otherwise, keep decrementing seconds and updating time on screen
+        */
+        function tick() {
+            if (totalSeconds == 0) {
+                timerFinish();
+                setStartButton(timerButton);
+            } else {
+                totalSeconds--;
+                display(totalSeconds, timerText);
+            }  
+        }
+        
+    }
 }
 
 customElements.define('pomo-timer', PomoTimer);
