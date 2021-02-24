bc898610f2c8b08bdbc78d973284549f9dc724c7
Steven Steiner <steets250@gmail.com>
2021-02-23 18:08:28 -0800

    Update Button References

--- source/components/pomo-settings.js
+++ source/components/pomo-settings.js
@@ -11,7 +11,7 @@ class PomoSettings extends HTMLElement {
             position: fixed;
             z-index: 1;
             top: 0;
-            left: 0px;
+            left: -500px;
             background-color: #000;
             overflow-x: hidden;
             transition: 0.5s;
@@ -43,28 +43,27 @@ class PomoSettings extends HTMLElement {
             float: right;
         }
         </style>`
-        const open = document.createElement('button');
-        open.setAttribute("id", "openSideBar");
-        open.setAttribute("style", "font-size:30px");
-        open.setAttribute("onclick", "openBar");
-        open.innerHTML = "&#9776;";
-        shadow.appendChild(open);
 
-        const settings = document.createElement('div');
-        settings.setAttribute("id", "settings");
+        this.open = document.createElement('button');
+        this.open.setAttribute("id", "openSideBar");
+        this.open.setAttribute("style", "font-size:30px");
+        this.open.innerHTML = "&#9776;";
+        shadow.appendChild(this.open);
 
-        const close = settings.appendChild(document.createElement("input"));
-        close.setAttribute("id", "closeSettings");
-        close.setAttribute("type", "button");
-        close.setAttribute("value", "X");
-        close.setAttribute("onclick", "closeBar");
+        this.settings = document.createElement('div');
+        this.settings.setAttribute("id", "settings");
 
-        const volume = settings.appendChild(document.createElement("div"));
+        this.close = this.settings.appendChild(document.createElement("input"));
+        this.close.setAttribute("id", "closeSettings");
+        this.close.setAttribute("type", "button");
+        this.close.setAttribute("value", "X");
+
+        const volume = this.settings.appendChild(document.createElement("div"));
         volume.innerHTML = 
           `<label>Volume</label>
           <input type="range" id="volume" min="0" max="100" value="100">`;
 
-        const sound = settings.appendChild(document.createElement("div"));
+        const sound = this.settings.appendChild(document.createElement("div"));
         sound.innerHTML = 
           `<label for="soundOptions">Sound</label>
           <select id="soundOptions">
@@ -73,28 +72,27 @@ class PomoSettings extends HTMLElement {
               <option value="Meow">Meow</option>
           </select>`;
 
-        const calm = settings.appendChild(document.createElement("div"));
+        const calm = this.settings.appendChild(document.createElement("div"));
         calm.innerHTML = `<label for="calm">Calm Mode</label>`;
-        shadow.appendChild(settings);
+        shadow.appendChild(this.settings);
+
+        this.open.addEventListener('click', () => {
+          this.openBar();
+        });
+
+        this.close.addEventListener('click', () => {
+          this.closeBar();
+        });
+      }
+
+      openBar = () => {
+        this.settings.style.left = "0";
+      }
+      
+      closeBar = () => {
+        this.settings.style.left = "-500px";
       }
 }
-
-const openBar = () => {
-  document.getElementById("sideBar").style.left = "0";
-}
-
-const closeBar = () => {
-  document.getElementById("settings").style.left = "-500px";
-}
-
-document.addEventListener('click', (e) => {
-  let settings = document.getElementById('settings');
-  let openBtn = document.getElementById('openSideBar');
-  if (!settings.contains(e.target) && !(e.target == openBtn)) {
-      closeBar;
-  }
-})
-
 
 customElements.define('pomo-settings', PomoSettings);
 
