25d467f0348ae5d335159d0d43c9b491e0257c5a
Richard Duong <riduong@ucsd.edu>
2021-03-04 00:28:58 -0800

    Added Jest testing for storage

--- source/storage.js
+++ source/storage.js
@@ -1,1 +1,179 @@
 console.log("Storage loaded!");
+
+/* Functions for Local Storage updates */
+
+/**
+ * Function to get the current day and check if it should reset local storage
+ */
+function getDayCount() {
+  if(!(localStorage.getItem('pomodoroCount'))) {
+    setDayCount(0);
+  }
+  // Clears local storage for the day if its a new day and past 4am
+  var date = new Date();
+  if (date.getFullYear() > localStorage.getItem('prevYear') || date.getMonth()+1 > localStorage.getItem('prevMonth') || date.getDate() > localStorage.getItem('prevDay')) {
+    if (date.getHours() >= 4) {
+      localStorage.setItem('prevYear',date.getFullYear());
+      localStorage.setItem('prevMonth',date.getMonth()+1);
+      localStorage.setItem('prevDay',date.getDate());
+      localStorage.setItem('pomodoroCount',0);
+    }
+  }
+  return parseInt(localStorage.getItem('pomodoroCount'));
+}
+
+/**
+ * Function to set the pomodoro completed for the day into local storage
+ * @param {Number} count Pomodoro completed
+ */
+function setDayCount(count) {
+  var date = new Date();
+  if(!(localStorage.getItem('prevDay'))) { // Creating new local storage for pomodoroCount if it is the first time
+  localStorage.setItem('prevYear',date.getFullYear());
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',0);
+  }
+  // Clears local storage for the day if its a new day and past 4am
+  if (date.getFullYear() > localStorage.getItem('prevYear') || date.getMonth()+1 > localStorage.getItem('prevMonth') || date.getDate() > localStorage.getItem('prevDay')) {
+    if (date.getHours() >= 4) {
+      localStorage.setItem('prevYear',date.getFullYear());
+      localStorage.setItem('prevMonth',date.getMonth()+1);
+      localStorage.setItem('prevDay',date.getDate());
+      localStorage.setItem('pomodoroCount',0);
+    }
+  }
+
+  localStorage.setItem('pomodoroCount',count + getDayCount());
+}
+
+/**
+ * Function to get the current Calm value
+ * *** Bug *** -> Doesn't work as a boolean
+ */
+function getCalm() {
+  if(!(localStorage.getItem('isCalm'))) { // Checking if there is a calm mode value stored in local storage
+      setCalm(false); // Creating local storage for calm mode if it doesn't exist
+  }
+  return eval(localStorage.getItem('isCalm'));
+
+}
+
+/**
+ * Function to be called whenever the onClick event is listened to for calm mode
+ * @param {Boolean} calm Setting of current calm mode
+ */
+function setCalm(calm) {
+  localStorage.setItem('isCalm',calm);
+}
+
+/**
+ * Function to get the current volume value
+ */
+function getVolume() {
+  if(!(localStorage.getItem('volume'))) { // Checking if there is a volume value stored in local storage
+    setVolume(50); // Setting default volume to be 50
+  }
+  return localStorage.getItem('volume');
+}
+  
+/**
+* Function to be called whenever the current volume is changed
+* @param {Integer} vol volume saved
+*/
+function setVolume(vol) {
+  localStorage.setItem('volume',vol);
+}
+
+/**
+ * Function to get the current sound
+ */
+function getSound() {
+  if(!(localStorage.getItem('sound'))) { // Checking if there is a sound value stored in local storage
+    setSound('bell'); // Setting default sound to be bell
+  }
+  return localStorage.getItem('sound');
+}
+  
+/**
+* Function to be called whenever the current sound is changed
+* @param {String} sound Alarm sound saved
+*/
+function setSound(sound) {
+  localStorage.setItem('sound',sound);
+}
+
+/**
+ * Function to get the current sound
+ */
+function getDark() {
+  if(!(localStorage.getItem('isDark'))) { // Checking if there is a dark mode value stored in local storage
+    setDark(false); // Setting default mode into light mode
+  }
+  return eval(localStorage.getItem('isDark'));
+}
+  
+/**
+ * Function to be called whenever the onClick event is listened to for dark mode
+ * @param {Boolean} dark Setting of current dark mode
+ */
+function setDark(dark) {
+  localStorage.setItem('isDark',dark);
+}
+
+/**
+ * Function to get the duration of the work timer in minutes
+ */
+function getWork() {
+  if(!(localStorage.getItem('work'))) { // Checking if the duration of work mode has been changed
+    setWork(25); // Setting default work mode to be 25 minutes
+  }
+  return localStorage.getItem('work');
+}
+    
+/**
+* Function to be called whenever the duration of work mode is changed
+* @param {Integer} work duration of new work mode
+*/
+function setWork(work) {
+  localStorage.setItem('work',work);
+}
+
+/**
+ * Function to get the duration of the short break timer in minutes
+ */
+function getShortBreak() {
+  if(!(localStorage.getItem('shortBreak'))) { // Checking if the duration of short break has been changed
+    setShortBreak(5); // Setting default short break to be 5 minutes
+  }
+  return localStorage.getItem('shortBreak');
+}
+      
+/**
+* Function to be called whenever the duration of short break is changed
+* @param {Integer} work duration of new short break
+*/
+function setShortBreak(shortBreak) {
+  localStorage.setItem('shortBreak',shortBreak);
+}
+
+/**
+ * Function to get the duration of the long break timer in minutes
+ */
+function getLongBreak() {
+  if(!(localStorage.getItem('longBreak'))) { // Checking if the duration of long break has been changed
+    setLongBreak(15); // Setting default long break to be 15 minutes
+  }
+  return localStorage.getItem('longBreak');
+}
+
+/**
+* Function to be called whenever the duration of long break is changed
+* @param {Integer} work duration of new long break
+*/
+function setLongBreak(longBreak) {
+  localStorage.setItem('longBreak',longBreak);
+}
+
+export {getDayCount,setDayCount,getCalm,setCalm,getVolume,setVolume,getSound,setSound,getDark,setDark,getWork,setWork,getShortBreak,setShortBreak,getLongBreak,setLongBreak}
+/* End of functions for Local Storage */
--- testing/jest/placeholder.js
+++ testing/jest/placeholder.js
@@ -1,3 +1,168 @@
-test("Returns sum of 1 and 2", () => {
-    expect(1 + 2).toBe(3);
+import { getDayCount, setDayCount, getCalm, setCalm, getVolume, setVolume, getSound, setSound, getDark, setDark, getWork, setWork, getShortBreak, setShortBreak, getLongBreak, setLongBreak } from '../../source/storage';
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+test('getDayCount without setDayCount', () => {
+  expect(getDayCount()).toBe(0);
+});
+
+test('getDayCount for same day/month/year', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',date.getFullYear());  
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',10);
+  expect(getDayCount()).toBe(10);
+});
+
+test('getDayCount for a new day', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',date.getFullYear());  
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',1);
+  localStorage.setItem('pomodoroCount',10);
+  expect(getDayCount()).toBe(0);
+});
+
+test('getDayCount for a new Month', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',date.getFullYear());  
+  localStorage.setItem('prevMonth',2);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',10);
+expect(getDayCount()).toBe(0);
+});
+
+test('getDayCount for a new Year', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',2020);  
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',10);
+  expect(getDayCount()).toBe(0);
+});
+
+test('setDayCount for the first time', () => {
+  setDayCount(5);
+  expect(getDayCount()).toBe(5);
+});
+
+test('setDayCount for same day/month/year', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',date.getFullYear());  
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',10);
+  setDayCount(5);
+  expect(getDayCount()).toBe(15);
+});
+
+test('setDayCount for a new day', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',date.getFullYear());  
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',1);
+  localStorage.setItem('pomodoroCount',10);
+  setDayCount(5);
+  expect(getDayCount()).toBe(5);
+});
+
+test('setDayCount for a new Month', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',date.getFullYear());  
+  localStorage.setItem('prevMonth',2);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',10);
+  setDayCount(11);
+  expect(getDayCount()).toBe(11);
+});
+
+test('setDayCount for a new Year', () => {
+  var date = new Date();
+  localStorage.setItem('prevYear',2020);  
+  localStorage.setItem('prevMonth',date.getMonth()+1);
+  localStorage.setItem('prevDay',date.getDate());
+  localStorage.setItem('pomodoroCount',10);
+  setDayCount(12);
+  expect(getDayCount()).toBe(12);
+});
+
+test('setCalm to be true', () => {
+    setCalm(true);
+    expect(getCalm()).toBe(true);
+  });
+
+test('getCalm without setCalm', () => {
+  expect(getCalm()).toBe(false);
+}); 
+
+test('setCalm to be true', () => {
+  setCalm(true);
+  expect(getCalm()).toBe(true);
+});
+
+test('setCalm to be false', () => {
+  setCalm(false);
+  expect(getCalm()).toBe(false);
+});
+
+test('getVolume without setVolume', () => {
+  expect(getVolume()).toBe(50);
+});
+
+test('setVolume to be 80', () => {
+  setVolume(80);
+  expect(getVolume()).toBe(80);
+});
+
+test('getSound without setSound', () => {
+  expect(getSound()).toBe('bell');
+});
+
+test('setSound to be jingle', () => {
+  setSound('jingle');
+  expect(getSound()).toBe('jingle');
+});
+
+test('getDark without setDark', () => {
+  expect(getDark()).toBe(false);
+});
+
+test('setDark to be true', () => {
+  setDark(true);
+  expect(getDark()).toBe(true);
+});
+
+test('setDark to be true', () => {
+  setDark(false);
+  expect(getDark()).toBe(false);
+});
+
+test('getWork without setWork', () => {
+  expect(getWork()).toBe(25);
+});
+
+test('setWork to be 30', () => {
+  setWork(30);
+  expect(getWork()).toBe(30);
+});
+
+test('getShortBreak without setShortBreak', () => {
+  expect(getShortBreak()).toBe(5);
+});
+  
+test('setShortBreak to be 10', () => {
+  setShortBreak(10);
+  expect(getShortBreak()).toBe(10);
+});
+
+test('getLongBreak without setLongBreak', () => {
+  expect(getLongBreak()).toBe(15);
+});
+    
+test('setLongBreak to be 20', () => {
+  setLongBreak(20);
+  expect(getLongBreak()).toBe(20);
 });
