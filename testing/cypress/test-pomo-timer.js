cb84aba2f60ee925025835895c1ed2c4a0e686f0
ntrappe <ntrappe@ucsd.edu>
2021-03-03 19:17:37 -0800

    using window to call functions and check vars works for timer

--- testing/cypress/test-pomo-timer.js
+++ testing/cypress/test-pomo-timer.js
@@ -30,8 +30,22 @@ describe('Find Timer Elements', { includeShadowDom: true }, () => {
   });
 });
 
-/*
-describe('Check Initial State of Elements', { includeShadowDom: true }, () => {
+describe('Call timer\'s public functions', { includeShadowDom: true }, () => {
+  let num = 12;
+
+  // SET WORK MODE
+  it('Call setTimer for work', () => {
+    cy.window().then((win) => {
+        win.pomoTimer.setTimer(num, 'work');
+    });
+  });
+
+  it('Check that total seconds is accurate', () => {
+    cy.window().then((win) => {
+        expect(win.pomoTimer.totalSeconds).to.eq(num * 60);
+    });
+  });
+
   it('Mode should be \'Work\'', () => {
     cy.get('#timer-mode').then(($el) => {
       expect($el).to.contain('WORK');
@@ -43,8 +57,35 @@ describe('Check Initial State of Elements', { includeShadowDom: true }, () => {
       expect($el).to.contain('Start');
     });
   });
+
+  // SET SHORT BREAK
+  num = 5;
+  it('Call setTimer for short break', () => {
+    cy.window().then((win) => {
+        win.pomoTimer.setTimer(num, 'short break');
+    });
+  });
+
+  it('Check that total seconds is accurate', () => {
+    cy.window().then((win) => {
+        expect(win.pomoTimer.totalSeconds).to.eq(num * 60);
+    });
+  });
+
+  it('Mode should be \'Short Break\'', () => {
+    cy.get('#timer-mode').then(($el) => {
+      expect($el).to.contain('SHORT');
+    });
+  });
+
+  it('Button should be \'Start\'', () => {
+    cy.get('#timer-button').then(($el) => {
+      expect($el).to.contain('Start');
+    });
+  });
 });
 
+/*
 describe('Basic Button Toggles', { includeShadowDom: true }, () => {
   it('Button toggles when Start clicked', () => {
     cy.get('#timer-button').click();
