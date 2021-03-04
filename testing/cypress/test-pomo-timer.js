49396fc897542122ed9dd92f00502698b3b0c12b
ntrappe <33298966+ntrappe@users.noreply.github.com>
2021-03-03 22:02:44 -0800

    Basic timer functionality (#67)
    * Update Control + Starter
    
    * basic timer sin css
    
    * basic timer fxn and changed test timer IDs
    
    * Steven add event listener
    
    * using window to call functions and check vars works for timer
    
    * timer tests calling functions
    
    Co-authored-by: Steven Steiner <steets250@gmail.com>

--- testing/cypress/test-pomo-timer.js
+++ testing/cypress/test-pomo-timer.js
@@ -16,10 +16,9 @@ describe('Find Timer Element with JS', () => {
 });
 
 /* Add your own tests here */
-
 describe('Find Timer Elements', { includeShadowDom: true }, () => {
   it('Get mode text via ID', () => {
-    cy.get('#mode');
+    cy.get('#timer-mode');
   });
 
   it('Get timer text via class', () => {
@@ -27,36 +26,78 @@ describe('Find Timer Elements', { includeShadowDom: true }, () => {
   });
 
   it('Get toggle button via ID', () => {
-    cy.get('#button');
+    cy.get('#timer-button');
   });
 });
 
-describe('Check Initial State of Elements', { includeShadowDom: true }, () => {
+describe('Call timer\'s public functions', { includeShadowDom: true }, () => {
+  let num = 12;
+
+  // SET WORK MODE
+  it('Call setTimer for work', () => {
+    cy.window().then((win) => {
+        win.pomoTimer.setTimer(num, 'work');
+    });
+  });
+
+  it('Check that total seconds is accurate', () => {
+    cy.window().then((win) => {
+        expect(win.pomoTimer.totalSeconds).to.eq(num * 60);
+    });
+  });
+
   it('Mode should be \'Work\'', () => {
-    cy.get('#mode').then(($el) => {
+    cy.get('#timer-mode').then(($el) => {
       expect($el).to.contain('WORK');
     });
   });
 
   it('Button should be \'Start\'', () => {
-    cy.get('#button').then(($el) => {
+    cy.get('#timer-button').then(($el) => {
+      expect($el).to.contain('Start');
+    });
+  });
+
+  // SET SHORT BREAK
+  num = 5;
+  it('Call setTimer for short break', () => {
+    cy.window().then((win) => {
+        win.pomoTimer.setTimer(num, 'short break');
+    });
+  });
+
+  it('Check that total seconds is accurate', () => {
+    cy.window().then((win) => {
+        expect(win.pomoTimer.totalSeconds).to.eq(num * 60);
+    });
+  });
+
+  it('Mode should be \'Short Break\'', () => {
+    cy.get('#timer-mode').then(($el) => {
+      expect($el).to.contain('SHORT');
+    });
+  });
+
+  it('Button should be \'Start\'', () => {
+    cy.get('#timer-button').then(($el) => {
       expect($el).to.contain('Start');
     });
   });
 });
 
+/*
 describe('Basic Button Toggles', { includeShadowDom: true }, () => {
   it('Button toggles when Start clicked', () => {
-    cy.get('#button').click();
-    cy.get('#button').then(($el) => {
+    cy.get('#timer-button').click();
+    cy.get('#timer-button').then(($el) => {
       expect($el).to.have.attr('class', 'reset');
     });
   });
 
   it('Button toggles when Reset clicked', () => {
-    cy.get('#button').click();
-    cy.get('#button').then(($el) => {
+    cy.get('#timer-button').click();
+    cy.get('#timer-button').then(($el) => {
       expect($el).to.have.attr('class', 'start');
     });
   });
-});
+});*/
