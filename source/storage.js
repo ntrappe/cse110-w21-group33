7ee0473a2f8d76e9330f7db228c4215958b0b453
Richard Duong <riduong@ucsd.edu>
2021-03-09 18:16:21 -0800

    Removed Magic Numbers, Added return values, and Make lines less than 100 long

--- source/storage.js
+++ source/storage.js
@@ -1,52 +1,66 @@
 console.log("Storage loaded!");
 
+const DEFAULT_WORK = 25;
+const DEFAULT_SHORT_BREAK = 5;
+const DEFAULT_LONG_BREAK = 15;
+const EMPTY_VALUE = 0;
+
 /* Functions for Local Storage updates */
 
 /**
  * Function to get the pomodoro completed for the day and check if it should reset local storage
+ * @return {Number} total amount of Pomodoro completed for the day
  */
 function getDayCount() {
-  if(!(localStorage.getItem('pomodoroCount'))) { // If local storage for pomodro completed is does not exist then we set current count to 0
+  // If local storage for pomodro completed is does not exist then we set current count to 0
+  if(!(localStorage.getItem('pomodoroCount'))) {
     setDayCount(0);
   }
   // Clears local storage for the day if its a new day
-  var date = new Date();
-  if (date.getFullYear() > localStorage.getItem('prevYear') || date.getMonth()+1 > localStorage.getItem('prevMonth') || date.getDate() > localStorage.getItem('prevDay')) {
+  let date = new Date();
+  if (date.getFullYear() > localStorage.getItem('prevYear') ||
+  date.getMonth()+1 > localStorage.getItem('prevMonth') ||
+  date.getDate() > localStorage.getItem('prevDay')) {
     localStorage.setItem('prevYear',date.getFullYear());
     localStorage.setItem('prevMonth',date.getMonth()+1);
     localStorage.setItem('prevDay',date.getDate());
-    localStorage.setItem('pomodoroCount',0);
+    localStorage.setItem('pomodoroCount',EMPTY_VALUE);
   }
   return parseInt(localStorage.getItem('pomodoroCount'));
 }
 
 /**
  * Function to set the pomodoro completed for the day into local storage
- * @param {Number} count Pomodoro completed
+ * @param {Number} count pomodoro completed
  */
 function setDayCount(count) {
-  var date = new Date();
-  if(!(localStorage.getItem('prevDay'))) { // Creating new local storage for pomodoroCount if it is the first time
+  let date = new Date();
+  // Creating new local storage for pomodoroCount if it is the first time
+  if(!(localStorage.getItem('prevDay'))) {
     localStorage.setItem('prevYear',date.getFullYear());
     localStorage.setItem('prevMonth',date.getMonth()+1);
     localStorage.setItem('prevDay',date.getDate());
-    localStorage.setItem('pomodoroCount',0);
+    localStorage.setItem('pomodoroCount',EMPTY_VALUE);
   }
   // Clears local storage for the day if its a new day
-  if (date.getFullYear() > localStorage.getItem('prevYear') || date.getMonth()+1 > localStorage.getItem('prevMonth') || date.getDate() > localStorage.getItem('prevDay')) {
+  if (date.getFullYear() > localStorage.getItem('prevYear') ||
+  date.getMonth()+1 > localStorage.getItem('prevMonth') ||
+  date.getDate() > localStorage.getItem('prevDay')) {
     localStorage.setItem('prevYear',date.getFullYear());
     localStorage.setItem('prevMonth',date.getMonth()+1);
     localStorage.setItem('prevDay',date.getDate());
-    localStorage.setItem('pomodoroCount',0);
+    localStorage.setItem('pomodoroCount',EMPTY_VALUE);
   }
   localStorage.setItem('pomodoroCount',count + getDayCount());
 }
 
 /**
  * Function to get the current Calm value
+ * @return {Boolean} boolean value of whether the user wanted calm more
  */
 function getCalm() {
-  if(!(localStorage.getItem('isCalm'))) { // Checking if there is a calm mode value stored in local storage
+  // Checking if there is a calm mode value stored in local storage
+  if(!(localStorage.getItem('isCalm'))) {
       setCalm(false); // Creating local storage for calm mode if it doesn't exist
   }
   return eval(localStorage.getItem('isCalm'));
@@ -55,7 +69,7 @@ function getCalm() {
 
 /**
  * Function to be called whenever the onClick event is listened to for calm mode
- * @param {Boolean} calm Setting of current calm mode
+ * @param {Boolean} calm setting of current calm mode
  */
 function setCalm(calm) {
   localStorage.setItem('isCalm',calm);
@@ -63,17 +77,19 @@ function setCalm(calm) {
 
 /**
  * Function to get the current volume value
+ * @return {Number} value of last saved volume
  */
 function getVolume() {
-  if(!(localStorage.getItem('volume'))) { // Checking if there is a volume value stored in local storage
-    setVolume(100); // Setting default volume to be 50
+  // Checking if there is a volume value stored in local storage
+  if(!(localStorage.getItem('volume'))) {
+    setVolume(100); // setting default volume to be 50
   }
   return eval(localStorage.getItem('volume'));
 }
   
 /**
 * Function to be called whenever the current volume is changed
-* @param {Integer} vol volume saved
+* @param {Number} vol volume saved
 */
 function setVolume(vol) {
   localStorage.setItem('volume',vol);
@@ -81,17 +97,19 @@ function setVolume(vol) {
 
 /**
  * Function to get the current sound
+ * @return {String} last saved sound for alarm
  */
 function getSound() {
-  if(!(localStorage.getItem('sound'))) { // Checking if there is a sound value stored in local storage
-    setSound('zapsplat_household_alarm_clock_old_fashioned_ring_very_short_44062.mp3'); // Setting default sound to be bell
+  if(!(localStorage.getItem('sound'))) { // Checking if sound local storage was created
+    //default sound
+    setSound('zapsplat_household_alarm_clock_old_fashioned_ring_very_short_44062.mp3');
   }
   return localStorage.getItem('sound');
 }
   
 /**
 * Function to be called whenever the current sound is changed
-* @param {String} sound Alarm sound saved
+* @param {String} sound alarm sound saved
 */
 function setSound(sound) {
   localStorage.setItem('sound',sound);
@@ -99,9 +117,10 @@ function setSound(sound) {
 
 /**
  * Function to get the current sound
+ * @return {Boolean} boolean value of whether or not dark mode is enabled
  */
 function getDark() {
-  if(!(localStorage.getItem('isDark'))) { // Checking if there is a dark mode value stored in local storage
+  if(!(localStorage.getItem('isDark'))) { // Checking if dark mode local storage was created
     setDark(false); // Setting default mode into light mode
   }
   return eval(localStorage.getItem('isDark'));
@@ -117,17 +136,18 @@ function setDark(dark) {
 
 /**
  * Function to get the duration of the work timer in minutes
+ * @return {Number} saved value of work mode duration
  */
 function getWork() {
   if(!(localStorage.getItem('work'))) { // Checking if the duration of work mode has been changed
-    setWork(25); // Setting default work mode to be 25 minutes
+    setWork(DEFAULT_WORK); // Setting default work mode to be 25 minutes
   }
   return eval(localStorage.getItem('work'));
 }
     
 /**
 * Function to be called whenever the duration of work mode is changed
-* @param {Integer} work duration of new work mode
+* @param {Number} work duration of new work mode
 */
 function setWork(work) {
   localStorage.setItem('work',work);
@@ -135,17 +155,19 @@ function setWork(work) {
 
 /**
  * Function to get the duration of the short break timer in minutes
+ * @return {Number} saved value of short break duration
  */
 function getShortBreak() {
-  if(!(localStorage.getItem('shortBreak'))) { // Checking if the duration of short break has been changed
-    setShortBreak(5); // Setting default short break to be 5 minutes
+  // Checking if the duration of short break has been changed
+  if(!(localStorage.getItem('shortBreak'))) {
+    setShortBreak(DEFAULT_SHORT_BREAK); // Setting default short break to be 5 minutes
   }
   return eval(localStorage.getItem('shortBreak'));
 }
       
 /**
 * Function to be called whenever the duration of short break is changed
-* @param {Integer} work duration of new short break
+* @param {Number} work duration of new short break
 */
 function setShortBreak(shortBreak) {
   localStorage.setItem('shortBreak',shortBreak);
@@ -153,17 +175,19 @@ function setShortBreak(shortBreak) {
 
 /**
  * Function to get the duration of the long break timer in minutes
+ * @return {Number} saved value of long break duration
  */
 function getLongBreak() {
-  if(!(localStorage.getItem('longBreak'))) { // Checking if the duration of long break has been changed
-    setLongBreak(15); // Setting default long break to be 15 minutes
+  // Checking if the duration of long break has been changed
+  if(!(localStorage.getItem('longBreak'))) {
+    setLongBreak(DEFAULT_LONG_BREAK); // Setting default long break to be 15 minutes
   }
   return eval(localStorage.getItem('longBreak'));
 }
 
 /**
 * Function to be called whenever the duration of long break is changed
-* @param {Integer} work duration of new long break
+* @param {Number} work duration of new long break
 */
 function setLongBreak(longBreak) {
   localStorage.setItem('longBreak',longBreak);
