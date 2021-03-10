082e3883de7b60642da402d335d5b3583fe51386
Steven Steiner <steets250@gmail.com>
2021-03-10 14:28:37 -0800

    Missing Deps + Linting Fixes

--- source/components/pomo-timer.js
+++ source/components/pomo-timer.js
@@ -1,67 +1,66 @@
 const START = 'Start';
 const RESET = 'Reset';
+
 class PomoTimer extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        const shadow = this.attachShadow({mode: 'open'});
+    const shadow = this.attachShadow({ mode: 'open' });
 
-        const wrapper = document.createElement('span');
-        wrapper.setAttribute('class', 'wrapper');
+    const wrapper = document.createElement('span');
+    wrapper.setAttribute('class', 'wrapper');
 
-        // label for mode e.g. 'Work'
-        let currentMode = document.createElement('p');          
-        currentMode.setAttribute('id', 'mode'); 
-        currentMode.textContent = "WORK";
+    // label for mode e.g. 'Work'
+    const currentMode = document.createElement('p');
+    currentMode.setAttribute('id', 'mode');
+    currentMode.textContent = 'WORK';
 
-        // timer countdown display
-        let timerText = document.createElement('h1')            
-        timerText.setAttribute('class', 'time');
-        timerText.setAttribute('id', 'timer-text');
-        timerText.textContent = "TIME";
+    // timer countdown display
+    const timerText = document.createElement('h1');
+    timerText.setAttribute('class', 'time');
+    timerText.setAttribute('id', 'timer-text');
+    timerText.textContent = 'TIME';
 
-        // timer button
-        let timerButton = document.createElement('button'); 
-        timerButton.setAttribute('id', 'button');
-        timerButton.setAttribute('class', 'start');
-        timerButton.textContent = START; 
+    // timer button
+    const timerButton = document.createElement('button');
+    timerButton.setAttribute('id', 'button');
+    timerButton.setAttribute('class', 'start');
+    timerButton.textContent = START;
 
-        shadow.appendChild(wrapper);
-        wrapper.appendChild(currentMode);
-        wrapper.appendChild(timerText);
-        wrapper.appendChild(timerButton);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(currentMode);
+    wrapper.appendChild(timerText);
+    wrapper.appendChild(timerButton);
 
-        /**
-         * Function calls to toggle button and control timer based on user input
-         */
-        timerButton.onclick = () => {
-            if (timerButton.textContent === START) {
-                setResetButton(timerButton);
-            } else {
-                setStartButton(timerButton);
-            }
-        };
-
-        /**
-         * Changes button to represent Start
-         *  @param {Button} timerButton button for Start/Reset timer
-         */
-        function setStartButton(timerButton) {
-            timerButton.innerHTML = START;
-            timerButton.setAttribute('class', 'start');
-        }
-
-        /**
-         * Changes button to represent Reset
-         *  @param {Button} timerButton button for Start/Reset timer
-         */
-        function setResetButton(timerButton) {
-            timerButton.innerHTML = RESET;
-            timerButton.setAttribute('class', 'reset');
-        }
+    /**
+     * Changes button to represent Start
+     */
+    function setStartButton() {
+      timerButton.innerHTML = START;
+      timerButton.setAttribute('class', 'start');
     }
+
+    /**
+     * Changes button to represent Reset
+     */
+    function setResetButton() {
+      timerButton.innerHTML = RESET;
+      timerButton.setAttribute('class', 'reset');
+    }
+
+    /**
+     * Function calls to toggle button and control timer based on user input
+     */
+    timerButton.onclick = () => {
+      if (timerButton.textContent === START) {
+        setResetButton();
+      } else {
+        setStartButton();
+      }
+    };
+  }
 }
 
 customElements.define('pomo-timer', PomoTimer);
 
-export { PomoTimer };
+export default PomoTimer;
