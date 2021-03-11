d751beb430e4860e0da147a214ac195c741e85f0
TurtleNinja <nhatvytruong54@gmail.com>
2021-03-11 14:38:31 -0800

    fix linter flags

--- source/components/pomo-finish.js
+++ source/components/pomo-finish.js
@@ -2,128 +2,139 @@ class PomoFinish extends HTMLElement {
   constructor() {
     super();
 
-        const shadow = this.attachShadow({mode: 'open'});
+    const shadow = this.attachShadow({ mode: 'open' });
         
-        // the component wrapper
-        const wrapper = document.createElement('div');
-        const link = document.createElement('link');
-        link.setAttribute('id', 'timer-styles');
-        link.setAttribute('rel', 'stylesheet');
-        link.setAttribute('href', './components/pomo-finish.css');
+    // the component wrapper
+    const wrapper = document.createElement('div');
+    const link = document.createElement('link');
+    link.setAttribute('id', 'timer-styles');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', './components/pomo-finish.css');
 
-        // custom event for modal display
-        this.event = new CustomEvent('modalRequest', {
-          bubbles: true,
-          composed: true
-        });
+    // custom event for modal display
+    this.event = new CustomEvent('modalRequest', {
+      bubbles: true,
+      composed: true
+    });
 
-        // button to finish session and display statistics
-        const finishButton = document.createElement('button');
-        finishButton.setAttribute('id', 'finish-button');
-        finishButton.textContent = "Statistics";
-        finishButton.onclick = () => { shadow.dispatchEvent(this.event) };
+    // button to finish session and display statistics
+    const finishButton = document.createElement('button');
+    finishButton.setAttribute('id', 'finish-button');
+    finishButton.textContent = 'Statistics';
+    finishButton.onclick = () => {
+      shadow.dispatchEvent(this.event)
+    };
 
-        // the lightbox
-        const modal = document.createElement('div');
-        modal.setAttribute('class', 'modal')
-        modal.setAttribute('id', 'statistics-modal');
-        modal.onclick = (event) => {
-          // close lightbox when click outside of the content area
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }
-
-        // wrapper for the content inside the lightbox
-        const modalContent = document.createElement('div');
-        modalContent.setAttribute('class', 'modal-content');
-        modalContent.setAttribute('id', 'statistics-modal-content');
-
-        // the main content to be display in the lightbox
-        // title
-        const modalTitle = document.createElement('h3');
-        modalTitle.setAttribute('class', 'modal-title');
-        modalTitle.setAttribute('id', 'statistics-modal-title');
-        modalTitle.innerHTML = "SESSION SUMMARY";
-        // list of stat
-
-        let sessionStatistics = document.createElement('ul');
-        sessionStatistics.setAttribute('id', 'statistics-panel');
-
-        // button to close the lightbox
-        let closeButton = document.createElement('div');
-        closeButton.setAttribute('id', 'statistics-close-button');
-        closeButton.setAttribute('class', 'button-off');
-        closeButton.innerHTML = "&times;";
-        closeButton.onclick = () => {
-          modal.style.display = "none";
-        }
-
-        /* mimic a button hover event */
-        closeButton.addEventListener('mouseover', () => {
-          closeButton.setAttribute('class', 'button-on');
-        });
-
-        closeButton.addEventListener('mouseout', () => {
-          closeButton.setAttribute('class', 'button-off');
-        });
-
-        // append elements to containers
-        modalContent.appendChild(closeButton);
-        modalContent.appendChild(modalTitle);
-        modalContent.appendChild(sessionStatistics);
-        modal.appendChild(modalContent);
-        wrapper.appendChild(modal);
-        wrapper.append(finishButton);
-        
-        shadow.appendChild(wrapper);
-        shadow.appendChild(link);
-
-        /**
-       * Render session's statistics to the screen
-       * @param {Number} workCount            the number of pomodoro sessions completed
-       * @param {Number} shortBreakCount      the number of short breaks
-       * @param {Number} longBreakCount       the number of long breaks
-       * @param {Number} interruptedCount     the number of time reset because of interruptions
-       * @param {Number} totalMinutesWorked   total number of minutes working
-       * @return {void} 
-       */
-        this.showModal = (workCount, shortBreakCount, longBreakCount, 
-                                interruptedCount, totalMinutesWorked) => {
-
-          sessionStatistics.innerHTML = "";  // clear the list before appending elements
-          
-          // render infomation
-          ["Pomodoro Completed", "Short Breaks", "Long Breaks",
-          "Interrupted Session", "Total Minutes Working"].forEach(function(info) {
-            let li = document.createElement('li');
-            li.setAttribute('class', 'session-statistics');
-            switch(info) {
-              case "Pomodoro Completed":
-                li.innerHTML = `${info}: ${workCount}`;
-                break;
-              case "Short Breaks":
-                li.innerHTML = `${info}: ${shortBreakCount}`;
-                break;
-              case "Long Breaks":
-                li.innerHTML = `${info}: ${longBreakCount}`;
-                break;
-              case "Interrupted Session":
-                li.innerHTML = `${info}: ${interruptedCount}`;
-                break;
-              case "Total Minutes Working":
-                li.innerHTML = `${info}: ${totalMinutesWorked}`;
-                break;
-              default:
-                li.innerHTML = "Buggy!";
-            }
-            sessionStatistics.appendChild(li);
-          });
-  
-          // show the statistics panel
-          modal.style.display = 'block';
-        }
+    // the lightbox
+    const modal = document.createElement('div');
+    modal.setAttribute('class', 'modal');
+    modal.setAttribute('id', 'statistics-modal');
+    modal.onclick = (event) => {
+      // close lightbox when click outside of the content area
+      if (event.target == modal) {
+        modal.style.display = 'none';
       }
+    };
+
+    // wrapper for the content inside the lightbox
+    const modalContent = document.createElement('div');
+    modalContent.setAttribute('class', 'modal-content');
+    modalContent.setAttribute('id', 'statistics-modal-content');
+
+    // the main content to be display in the lightbox
+    // title
+    const modalTitle = document.createElement('h3');
+    modalTitle.setAttribute('class', 'modal-title');
+    modalTitle.setAttribute('id', 'statistics-modal-title');
+    modalTitle.innerHTML = 'SESSION SUMMARY';
+    // list of stat
+    const sessionStatistics = document.createElement('ul');
+    sessionStatistics.setAttribute('id', 'statistics-panel');
+
+    // button to close the lightbox
+    const closeButton = document.createElement('div');
+    closeButton.setAttribute('id', 'statistics-close-button');
+    closeButton.setAttribute('class', 'button-off');
+    closeButton.innerHTML = '&times;';
+    closeButton.onclick = () => {
+      modal.style.display = 'none';
+    };
+
+    /* mimic a button hover event */
+    closeButton.addEventListener('mouseover', () => {
+      closeButton.setAttribute('class', 'button-on');
+    });
+
+    closeButton.addEventListener('mouseout', () => {
+      closeButton.setAttribute('class', 'button-off');
+    });
+
+    // append elements to containers
+    modalContent.appendChild(closeButton);
+    modalContent.appendChild(modalTitle);
+    modalContent.appendChild(sessionStatistics);
+    modal.appendChild(modalContent);
+    wrapper.appendChild(modal);
+    wrapper.append(finishButton);
+        
+    shadow.appendChild(wrapper);
+    shadow.appendChild(link);
+
+    /**
+     * Render session's statistics to the screen
+     * @param {Number} workCount            the number of pomodoro sessions completed
+     * @param {Number} shortBreakCount      the number of short breaks
+     * @param {Number} longBreakCount       the number of long breaks
+     * @param {Number} interruptedCount     the number of time reset because of interruptions
+     * @param {Number} totalMinutesWorked   total number of minutes working
+     * @return {void} 
+     */
+    this.showModal = (
+      workCount, 
+      shortBreakCount, 
+      longBreakCount, 
+      interruptedCount,
+      totalMinutesWorked
+    ) => {
+
+      sessionStatistics.innerHTML = '';  // clear the list before appending elements
+      
+      // render infomation
+      [
+        'Pomodoro Completed',
+        'Short Breaks',
+        'Long Breaks',
+        'Interrupted Session',
+        'Total Minutes Working'
+      ].forEach((info) => {
+        let li = document.createElement('li');
+        li.setAttribute('class', 'session-statistics');
+        switch(info) {
+          case 'Pomodoro Completed':
+            li.innerHTML = `${info}: ${workCount}`;
+            break;
+          case 'Short Breaks':
+            li.innerHTML = `${info}: ${shortBreakCount}`;
+            break;
+          case 'Long Breaks':
+            li.innerHTML = `${info}: ${longBreakCount}`;
+            break;
+          case 'Interrupted Session':
+            li.innerHTML = `${info}: ${interruptedCount}`;
+            break;
+          case 'Total Minutes Working':
+            li.innerHTML = `${info}: ${totalMinutesWorked}`;
+            break;
+          default:
+            li.innerHTML = 'Buggy!';
+        };
+        sessionStatistics.appendChild(li);
+      });
+
+      // show the statistics panel
+      modal.style.display = 'block';
+    };
+  }
 }
 
 customElements.define('pomo-finish', PomoFinish);
--- testing/cypress/test-pomo-finish.js
+++ testing/cypress/test-pomo-finish.js
@@ -6,111 +6,110 @@ describe('Open Page', () => {
 
 /* Starter Tests to find elements on page */
 describe('Find Finish Elements', { includeShadowDom: true }, () => {
-    beforeEach(() => {
-        cy.visit('./source/index.html');
-    });
-    it('Get element finish button', () => {
-        cy.get('#finish-button');
-    });
-    it('Get element close button', () => {
-        cy.get('#statistics-close-button');
-    });
-    it('Get lightbox title', () => {
-        cy.get('#statistics-modal-title');
-    });
-    it('Get element statistics panel', () => {
-        cy.get('#statistics-panel');
-    });
+  beforeEach(() => {
+    cy.visit('./source/index.html');
+  });
+  it('Get element finish button', () => {
+    cy.get('#finish-button');
+  });
+  it('Get element close button', () => {
+    cy.get('#statistics-close-button');
+  });
+  it('Get lightbox title', () => {
+    cy.get('#statistics-modal-title');
+  });
+  it('Get element statistics panel', () => {
+    cy.get('#statistics-panel');
+  });
 });
 describe('Check Initial State of Elements', { includeShadowDom: true }, () => {
-    beforeEach(() => {
-        cy.visit('./source/index.html');
+  beforeEach(() => {
+    cy.visit('./source/index.html');
+  });
+  it('Check that button labeled Statistics', () => {
+    cy.get('#finish-button').then(($el) => {
+        expect($el).to.contain('Statistics');
     });
-    it('Check that button labeled Statistics', () => {
-        cy.get('#finish-button').then(($el) => {
-            expect($el).to.contain('Statistics');
-        });
-    });
-    it('Check that lightbox is half transparent and is not displayed initially', () => {
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-        cy.get('#statistics-modal').should('have.css', 'background-color')
-        .and('eq', 'rgba(0, 0, 0, 0.5)');
-    });
+  });
+  it('Check that lightbox is half transparent and is not displayed initially', () => {
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+    cy.get('#statistics-modal').should('have.css', 'background-color')
+    .and('eq', 'rgba(0, 0, 0, 0.5)');
+  });
 });
 
 describe('Check Custom Event Dispatchment', { includeShadowDom: true }, () => {
-    it('Check that statistics button dispatch a custom event', () => {
-        cy.window().then((win) => {
-            // listen for events in the windown
-            win.addEventListener('modalRequest', (e) => {
-                expect($e).to.be.instanceOf(CustomEvent);
-            });
-          });
+  it('Check that statistics button dispatch a custom event', () => {
+    cy.window().then((win) => {
+      // listen for events in the windown
+      win.addEventListener('modalRequest', ($e) => {
+        expect($e).to.be.instanceOf(CustomEvent);
+      });
     });
+  });
 });
 
 describe('Check Lightbox Closing Options', { includeShadowDom: true }, () => {
-    beforeEach(() => {
-        cy.visit('./source/index.html');
-        cy.window().then((win) => {
-            win.pomoFinish.showModal(3, 2, 0, 0, 75);
-        });
+  beforeEach(() => {
+    cy.visit('./source/index.html');
+    cy.window().then((win) => {
+      win.pomoFinish.showModal(3, 2, 0, 0, 75);
     });
-    it('Check that close button closes lightbox', () => {
-        cy.get('#statistics-close-button').click().then(() => {
-            cy.get('#statistics-modal').should('have.css', 'display', 'none');
-        });
+  });
+  it('Check that close button closes lightbox', () => {
+    cy.get('#statistics-close-button')
+      .click()
+      .then(() => {
+      cy.get('#statistics-modal').should('have.css', 'display', 'none');
     });
-    it('Check clicking top of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('top');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking top right of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('topRight');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking top left of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('topLeft');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking bottom of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('bottom');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking bottom right of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('bottomRight');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking bottom left of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('bottomLeft');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking left of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('left');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
-    it('Check clicking right left of the modal closes lightbox', () => {
-        cy.get('#statistics-modal').click('right');
-        cy.get('#statistics-modal').should('have.css', 'display', 'none');
-    });
+  });
+  it('Check clicking top of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('top');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking top right of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('topRight');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking top left of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('topLeft');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking bottom of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('bottom');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking bottom right of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('bottomRight');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking bottom left of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('bottomLeft');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking left of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('left');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
+  it('Check clicking right left of the modal closes lightbox', () => {
+    cy.get('#statistics-modal').click('right');
+    cy.get('#statistics-modal').should('have.css', 'display', 'none');
+  });
 })
 
 describe('Check Statistics Panel Elements With showModal()', { includeShadowDom: true }, () => {
-    it('Check statistics panel has all elements', () => {
-        cy.visit('./source/index.html');
-        cy.window().then((win) => {
-            win.pomoFinish.showModal(3, 2, 0, 0, 75);
-        });
-        cy.get('#statistics-panel').then(($el) => {
-            expect($el).to.contain('Pomodoro Completed: 3');
-            expect($el).to.contain('Short Breaks: 2');
-            expect($el).to.contain('Long Breaks: 0');
-            expect($el).to.contain('Interrupted Session: 0');
-            expect($el).to.contain('Total Minutes Working: 75');
-        });
-        cy.get('#statistics-modal').should('have.css', 'display', 'block');
+  it('Check statistics panel has all elements', () => {
+    cy.visit('./source/index.html');
+    cy.window().then((win) => {
+      win.pomoFinish.showModal(3, 2, 0, 0, 75);
     });
+    cy.get('#statistics-panel').then(($el) => {
+      expect($el).to.contain('Pomodoro Completed: 3');
+      expect($el).to.contain('Short Breaks: 2');
+      expect($el).to.contain('Long Breaks: 0');
+      expect($el).to.contain('Interrupted Session: 0');
+      expect($el).to.contain('Total Minutes Working: 75');
+    });
+    cy.get('#statistics-modal').should('have.css', 'display', 'block');
+  });
 });
-
-
-/* Add your own tests here */
