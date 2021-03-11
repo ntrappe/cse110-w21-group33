6007ec1ac7d81dafba359ae7b85c4791a99112f9
IntelBear <bryantshao@gmail.com>
2021-03-10 23:04:48 -0800

    Changed Cypress test to hyphenated classes/ids

--- testing/cypress/test-pomo-info.js
+++ testing/cypress/test-pomo-info.js
@@ -7,17 +7,17 @@ describe("Open Page", () => {
 /* Tests to check if all elements exists in the shadow DOM */
 describe('Find Elements with ID', { includeShadowDom: true }, () => {
     it('Check for element (\'Info\')', () => {
-        cy.get('#infoButton').then(($el) => {
+        cy.get('#info-button').then(($el) => {
             expect($el).to.contain('Info');
         });
     });
     it('Check for close button', () => {
-        cy.get('#infoCloseButton').then(($el) => {
+        cy.get('#info-close-button').then(($el) => {
             expect($el).to.exist;
         });
     });
     it('Check for info content', () => {
-        cy.get('#infoContent').then(($el) => {
+        cy.get('#info-content').then(($el) => {
             expect($el).to.exist; // at least has a space
         });
     });
@@ -25,10 +25,10 @@ describe('Find Elements with ID', { includeShadowDom: true }, () => {
 
 describe('Check The Initial State of Component pomo-info', { includeShadowDom: true }, () => {
     it('Check that the lightbox is invisible', () => {
-        cy.get('#infoModal').should('have.css', 'display', 'none')
+        cy.get('#info-modal').should('have.css', 'display', 'none')
     });
     it('Check that the info button is enabled', () => {
-        cy.get('#infoButton').should('not.be.disabled');
+        cy.get('#info-button').should('not.be.disabled');
     });
 });
 
@@ -36,13 +36,13 @@ describe('Check The Enabling/Disabling Info Button', { includeShadowDom: true}, 
     it('Check that the button can be disabled', () => {
         cy.window().then((win) => {
             win.pomoInfo.disableInfo();
-            cy.get('#infoButton').should('be.disabled');
+            cy.get('#info-button').should('be.disabled');
         });
     });
     it('Check that the button can be enabled', () => {
         cy.window().then((win) => {
             win.pomoInfo.enableInfo();
-            cy.get('#infoButton').should('not.be.disabled');
+            cy.get('#info-button').should('not.be.disabled');
         });
     });
 })
@@ -53,57 +53,57 @@ describe('Check Ligthbox Controls', { includeShadowDom: true }, () => {
     });
 
     it('Check that Info button opens lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').should('have.css', 'display', 'block');
-        cy.get('#infoModal').should('have.css', 'background-color')
+        cy.get('#info-button').click();
+        cy.get('#info-modal').should('have.css', 'display', 'block');
+        cy.get('#info-modal').should('have.css', 'background-color')
         .and('eq', 'rgba(0, 0, 0, 0.5)');
     });
 
     it('Check that close button closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoCloseButton').click().then(() => {
-            cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-close-button').click().then(() => {
+            cy.get('#info-modal').should('have.css', 'display', 'none');
         });
     });
     it('Check clicking top of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('top');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('top');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking top right of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('topRight');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('topRight');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking top left of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('topLeft');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('topLeft');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking bottom of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('bottom');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('bottom');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking bottom right of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('bottomRight');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('bottomRight');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking bottom left of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('bottomLeft');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('bottomLeft');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking left of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('left');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('left');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
     it('Check clicking right left of the modal closes lightbox', () => {
-        cy.get('#infoButton').click();
-        cy.get('#infoModal').click('right');
-        cy.get('#infoModal').should('have.css', 'display', 'none');
+        cy.get('#info-button').click();
+        cy.get('#info-modal').click('right');
+        cy.get('#info-modal').should('have.css', 'display', 'none');
     });
 });
 
