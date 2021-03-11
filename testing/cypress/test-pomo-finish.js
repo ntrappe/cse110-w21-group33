c562f564db3d02fd0aef8c6da884835b79dca0d5
TurtleNinja <nhatvytruong54@gmail.com>
2021-03-10 17:56:48 -0800

    reset pomo-finish to match main

--- testing/cypress/test-pomo-finish.js
+++ testing/cypress/test-pomo-finish.js
@@ -10,24 +10,17 @@ describe("Open Page", () => {
 // Remove this when the component has been implemented
 describe('Find Finish Element with ID', { includeShadowDom: true }, () => {
     it('Check for element (\'Finish\')', () => {
-        cy.get('#finishButton').then(($el) => {
+        cy.get('p').then(($el) => {
             expect($el).to.contain('Finish');
         });
     });
 });
 
-describe('Check Statistics Panel Elements With showModal()', { includeShadowDom: true }, () => {
-    it('Check statistics panel has all elements', () => {
-        cy.visit('./source/index.html');
+// Used for calling functions on the component
+describe('Find Finish Element with JS', () => {
+    it('Get element (\'Finish\')', () => {
         cy.window().then((win) => {
-            win.pomoFinish.showModal(3, 2, 0, 75, 0);
-        });
-        cy.get('#statistics-panel').then(($el) => {
-            expect($el).to.contain('Pomodoro Completed: 3');
-            expect($el).to.contain('Short Breaks: 2');
-            expect($el).to.contain('Long Breaks: 0');
-            expect($el).to.contain('Interrupted Session: 0');
-            expect($el).to.contain('Total Minutes Working: 75');
+            expect(win.pomoFinish).to.exist;
         });
     });
 });
