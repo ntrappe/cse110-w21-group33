a60603c06741cb8ef6a4624057fe16d01c1d28fb
IntelBear <bryantshao@gmail.com>
2021-03-11 00:58:14 -0800

    Refactored initProgress test and added comments

--- testing/jest/jest-pomo-timer-helpers.js
+++ testing/jest/jest-pomo-timer-helpers.js
@@ -18,7 +18,7 @@ describe('test display', () => {
 });
 
 /* Test for function set */
-describe('test set', () => {
+describe('test setTime', () => {
   /* Test for abover or below max and min */
   test('set 0 and 60', () => {
     expect(pomoHelper.setTime(0)).toBe(120);
@@ -57,18 +57,22 @@ describe('test setResetButton', () => {
 describe('test initProgess', () => {
   test('initProgess', () => {
     const output = pomoHelper.initProgess();
-    /*
-    // @Bryant please refactor to match new pomo-timer-helpers code
-    expect(output.length).toBe(5);
+
+    /* Verify progress container contains all elements in expected order */
+    expect(output.length).toBe(7);
     expect(output[0].getAttribute('class')).toBe('progress-container');
-    expect(output[0].children[0].getAttribute('class')).toBe('progress-top-section');
-    expect(output[0].children[1].getAttribute('class')).toBe('progress-bottom-section');
 
-    expect(output[0].children[1].children[0]).toBe(output[1]);
-    expect(output[0].children[1].children[2]).toBe(output[2]);
-    expect(output[0].children[1].children[4]).toBe(output[3]);
-    expect(output[0].children[1].children[6]).toBe(output[4]);
+    /* Verify that squares and spaces are in right order */
+    expect(output[0].children[0]).toBe(output[1]);
+    expect(output[0].children[2]).toBe(output[2]);
+    expect(output[0].children[4]).toBe(output[3]);
+    expect(output[0].children[6]).toBe(output[4]);
 
+    expect(output[0].children[1].getAttribute('class')).toBe('space');
+    expect(output[0].children[3].getAttribute('class')).toBe('space');
+    expect(output[0].children[5].getAttribute('class')).toBe('space');
+
+    /* Verify that progress squares are initally off */
     expect(output[1].getAttribute('id')).toBe('square1');
     expect(output[2].getAttribute('id')).toBe('square2');
     expect(output[3].getAttribute('id')).toBe('square3');
@@ -78,11 +82,6 @@ describe('test initProgess', () => {
     expect(output[2].getAttribute('class')).toBe('square-off');
     expect(output[3].getAttribute('class')).toBe('square-off');
     expect(output[4].getAttribute('class')).toBe('square-off');
-
-    expect(output[0].children[1].children[1].getAttribute('class')).toBe('space');
-    expect(output[0].children[1].children[3].getAttribute('class')).toBe('space');
-    expect(output[0].children[1].children[5].getAttribute('class')).toBe('space');
-    */
   });
 });
 
@@ -92,6 +91,7 @@ describe('test setProgressHelper', () => {
   const output = pomoHelper.initProgess();
 
   test('setProgressHelper', () => {
+    /* Set progress bar to 1/4, 2/4, 3/4, and 4/4 and verify correct squares are on */
     pomoHelper.setProgressHelper(1, output[1], output[2], output[3], output[4]);
     expect(output[1].getAttribute('class')).toBe('square-on');
     expect(output[2].getAttribute('class')).toBe('square-off');
