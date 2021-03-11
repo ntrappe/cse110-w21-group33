8d5a75af54352f2e02b5058116884410d354bce7
IntelBear <bryantshao@gmail.com>
2021-03-10 23:09:59 -0800

    Removed showModal call in control because pomoFinish not implemented on this branch

--- source/control.js
+++ source/control.js
@@ -32,4 +32,4 @@ main.appendChild(pomoTimer);
 
 const footer = document.getElementById('footer');
 footer.appendChild(pomoFinish);
-pomoFinish.showModal(2,1,0,60,0);
+//pomoFinish.showModal(2,1,0,60,0);
