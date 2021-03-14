fd9026fe6b72a713d3e5b117679bb6bd8da5d56b
Vy Truong <nhatvytruong54@gmail.com>
2021-03-13 22:27:04 -0800

    Info page (#81)
    * Integrated finish button to component
    
    * Added close functionality
    
    * fix closeButton
    
    * fix pomo-finish styling
    
    * stat list not showing bug
    
    * added button styling for finish
    
    * add documentation
    
    * basic layout and functionality for pomo-info
    
    * add basic content
    
    * fix linting issue
    
    * beta for pomo-info unit tests
    
    * resolve merge conflict with pomo-finish unit tests
    
    * edit element's classes, css and unit tests
    
    * Added color check for infoModal
    
    * dummy edit to testing-pomo-finish to pass jobs
    
    * reset pomo-finish to match main
    
    * Changed IDs/classes to hyphenated
    
    * Changed Cypress test to hyphenated classes/ids
    
    * Removed showModal call in control because pomoFinish not implemented on this branch
    
    * fix linter flags
    
    * fix linter issues
    
    * Revert package-lock.json
    
    * Remove Extra Files
    
    * Restore CSS
    
    * refactor pomo-info.css to light and dark
    
    * add and test setDark()
    
    * remove data-mode changes from setDark()
    
    Co-authored-by: Michael Donaldson <xmichaeld1@gmail.com>
    Co-authored-by: Steven Steiner <steets250@gmail.com>
    Co-authored-by: ntrappe <ntrappe@ucsd.edu>
    Co-authored-by: IntelBear <bryantshao@gmail.com>

--- source/components/pomo-info.js
+++ source/components/pomo-info.js
@@ -4,11 +4,109 @@ class PomoInfo extends HTMLElement {
 
     const shadow = this.attachShadow({ mode: 'open' });
 
-    const element = document.createElement('p');
+    // wrapper for element
+    const wrapper = document.createElement('div');
 
-    element.textContent = 'Info';
+    // style element for the component
+    const link = document.createElement('link');
+    link.setAttribute('id', 'info-timer-styles');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', './components/pomo-info-dark.css');
 
-    shadow.appendChild(element);
+    // the modal window
+    const modal = document.createElement('div');
+    modal.setAttribute('class', 'modal');
+    modal.setAttribute('id', 'info-modal');
+    modal.style.display = 'none';
+    modal.onclick = (event) => {
+      // close lightbox when click outside of the content area
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    };
+
+    // info button
+    const infoButton = document.createElement('button');
+    infoButton.setAttribute('id', 'info-button');
+    infoButton.onclick = () => {
+      modal.style.display = 'block';
+    };
+    infoButton.innerHTML = 'Info';
+
+    // the lightbox
+    const modalContent = document.createElement('div');
+    modalContent.setAttribute('class', 'modal-content');
+    modalContent.setAttribute('id', 'info-modal-content');
+
+    // the main info Content
+    const infoContent = document.createElement('div');
+    infoContent.setAttribute('id', 'info-content');
+    const title = document.createElement('h3');
+    const content = document.createElement('div');
+    title.innerHTML = 'HOW IT WORKS';
+    content.innerHTML = `The Pomodoro Technique is a time 
+    management method developed by Francesco Cirillo in the 
+    late 1980s. The technique uses a timer to break down work
+    into intervals, traditionally 25 minutes in length, 
+    separated by short breaks, traditionally 5 minutes. After 
+    4 working intervals, the break is 15 minutes`;
+
+    // button to close the modal window
+    const closeButton = document.createElement('div');
+    closeButton.setAttribute('class', 'close-button');
+    closeButton.setAttribute('id', 'info-close-button');
+    closeButton.setAttribute('class', 'button-off');
+    closeButton.onclick = () => {
+      modal.style.display = 'none';
+    };
+    closeButton.innerHTML = '&times;';
+
+    /* mimic a button hover event */
+    closeButton.addEventListener('mouseover', () => {
+      closeButton.setAttribute('class', 'button-on');
+    });
+
+    closeButton.addEventListener('mouseout', () => {
+      closeButton.setAttribute('class', 'button-off');
+    });
+
+    // add element to containers
+    infoContent.appendChild(title);
+    infoContent.appendChild(content);
+    modalContent.appendChild(closeButton);
+    modalContent.appendChild(infoContent);
+    modal.appendChild(modalContent);
+    wrapper.appendChild(modal);
+    wrapper.appendChild(infoButton);
+
+    shadow.appendChild(wrapper);
+    shadow.appendChild(link);
+
+    /**
+     * Allows the control to open the info page
+     */
+    this.enableInfo = () => {
+      infoButton.disabled = false;
+    };
+
+    /**
+     * Prevent the control from open the info page
+     */
+    this.disableInfo = () => {
+      infoButton.disabled = true;
+    };
+
+    /**
+     * Modify elements' data-mode to dark-mode or light-mode
+     * @param {Boolean} isDark  indicate whether or not the setting is in dark mode
+     */
+    this.setDark = (isDark) => {
+      if (isDark) {
+        link.setAttribute('href', './components/pomo-info-dark.css');
+      } else {
+        link.setAttribute('href', './components/pomo-info-light.css');
+      }
+    };
   }
 }
 
