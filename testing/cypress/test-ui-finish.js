b7f46d736ee17f006741509b1cf1b4a1d7216a7b
IntelBear <bryantshao@gmail.com>
2021-03-16 10:48:49 -0700

    Added light mode tests for info/finish

--- testing/cypress/test-ui-finish.js
+++ testing/cypress/test-ui-finish.js
@@ -4,14 +4,14 @@ describe('Open Page', () => {
   });
 });
 
-describe('Verify setDark colors for pomoFinish', { includeShadowDom: true }, () => {
+describe('Verify dark mode colors for pomoFinish', { includeShadowDom: true }, () => {
   it('Check if pomoFinish exists', () => {
     cy.window().then((win) => {
       expect(win.pomoFinish).to.exist;
     });
   });
 
-  it('Check setDark colors', () => {
+  it('Check dark mode colors', () => {
     cy.window().then((win) => {
       win.pomoFinish.setDark(true);
 
@@ -19,12 +19,31 @@ describe('Verify setDark colors for pomoFinish', { includeShadowDom: true }, () 
 
       /* Verify colors of finish button */
       cy.get('#finish-button').should('have.css', 'background-color', 'rgb(34, 38, 44)');
-      cy.get('#finish-button').should('have.css', 'color', 'rgb(203, 209, 216)');
       cy.get('#finish-button').should('have.css', 'border-color', 'rgb(49, 54, 60)');
+      cy.get('#finish-button').should('have.css', 'color', 'rgb(203, 209, 216)');
 
       /* Verify background colors of stats modal */
       cy.get('#statistics-modal').should('have.css', 'background-color', 'rgba(0, 0, 0, 0.5)');
       cy.get('#statistics-modal-content').should('have.css', 'background-color', 'rgb(15, 17, 21)');
+      cy.get('#statistics-modal-content').should('have.css', 'border-color', 'rgb(49, 54, 60)');
+    });
+  });
+});
+
+describe('Verify light mode colors for pomoFinish', { includeShadowDom: true }, () => {
+  it('Check light mode colors', () => {
+    cy.window().then((win) => {
+      win.pomoFinish.setDark(false);
+
+      /* Verify colors of finish button */
+      cy.get('#finish-button').should('have.css', 'background-color', 'rgb(229, 231, 234)');
+      cy.get('#finish-button').should('have.css', 'border-color', 'rgb(216, 219, 220)');
+      cy.get('#finish-button').should('have.css', 'color', 'rgb(36, 40, 44)');
+
+      /* Verify background colors of stats modal */
+      cy.get('#statistics-modal').should('have.css', 'background-color', 'rgba(0, 0, 0, 0.5)');
+      cy.get('#statistics-modal-content').should('have.css', 'background-color', 'rgb(15, 17, 21)');
+      cy.get('#statistics-modal-content').should('have.css', 'border-color', 'rgb(49, 54, 60)');
     });
   });
 });
--- testing/cypress/test-ui-info.js
+++ testing/cypress/test-ui-info.js
@@ -4,27 +4,56 @@ describe('Open Page', () => {
   });
 });
 
-describe('Verify setDark colors for pomoInfo', { includeShadowDom: true }, () => {
+describe('Verify dark mode colors for pomoInfo', { includeShadowDom: true }, () => {
   it('Check if pomoInfo exists', () => {
     cy.window().then((win) => {
       expect(win.pomoInfo).to.exist;
     });
   });
 
-  it('Check setDark colors', () => {
+  it('Check dark mode colors', () => {
     cy.window().then((win) => {
       win.pomoInfo.setDark(true);
 
       cy.get('#info-button').click();
 
-      /* Verify colors of info button */
+      /* Verify colors of info buttons */
       cy.get('#info-button').should('have.css', 'background-color', 'rgb(34, 38, 44)');
-      cy.get('#info-button').should('have.css', 'color', 'rgb(203, 209, 216)');
       cy.get('#info-button').should('have.css', 'border-color', 'rgb(49, 54, 60)');
+      cy.get('#info-button').should('have.css', 'color', 'rgb(203, 209, 216)');
+      cy.get('#info-close-button').should('have.css', 'background-color', 'rgb(14, 17, 22)');
+      cy.get('#info-close-button').should('have.css', 'border-color', 'rgb(49, 54, 60)');
+      cy.get('#info-close-button').should('have.css', 'color', 'rgb(203, 209, 216)');
 
       /* Verify background colors of info modal */
       cy.get('#info-modal').should('have.css', 'background-color', 'rgba(0, 0, 0, 0.5)');
       cy.get('#info-content').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+      cy.get('#info-content').should('have.css', 'border-bottom', '0px none rgb(203, 209, 216)');
+      cy.get('#info-modal-content').should('have.css', 'background-color', 'rgb(23, 27, 33)');
+      cy.get('#info-modal-content').should('have.css', 'border-color', 'rgb(49, 54, 60)');
+    });
+  });
+});
+
+describe('Verify light mode colors for pomoInfo', { includeShadowDom: true }, () => {
+  it('Check light mode colors', () => {
+    cy.window().then((win) => {
+      win.pomoInfo.setDark(false);
+
+      /* Verify colors of info buttons */
+      cy.get('#info-button').should('have.css', 'background-color', 'rgb(229, 231, 234)');
+      cy.get('#info-button').should('have.css', 'border-color', 'rgb(216, 219, 220)');
+      cy.get('#info-button').should('have.css', 'color', 'rgb(36, 40, 44)');
+      cy.get('#info-close-button').should('have.css', 'background-color', 'rgb(14, 17, 22)');
+      cy.get('#info-close-button').should('have.css', 'border-color', 'rgb(49, 54, 60)');
+      cy.get('#info-close-button').should('have.css', 'color', 'rgb(203, 209, 216)');
+
+      /* Verify background colors of info modal */
+      cy.get('#info-modal').should('have.css', 'background-color', 'rgba(0, 0, 0, 0.5)');
+      cy.get('#info-content').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+      cy.get('#info-content').should('have.css', 'border-bottom', '0px none rgb(203, 209, 216)');
+      cy.get('#info-modal-content').should('have.css', 'background-color', 'rgb(23, 27, 33)');
+      cy.get('#info-modal-content').should('have.css', 'border-color', 'rgb(49, 54, 60)');
     });
   });
 });
