969b421d6b6dcb58e455eee91f91ec976429783e
Steven Steiner <steets250@gmail.com>
2021-03-18 20:59:29 -0700

    Control Module (#103)
    * Initial Control Code
    
    * Initial Control Code Take 2
    
    * Instant Settings Updates
    
    * Update Light Styling
    
    * Fixed storage testing to reflect new storage
    
    * Revert package-lock.json
    
    * Save and Load Mode
    
    * Control Re-structuring
    
    * Minor Cleaning
    
    * Mode Enum
    
    * Accessibility
    
    * Fix Disable Typo
    
    * Thanks Richard
    
    * I Love Chad
    
    * Fixing Default Audio
    
    * Fix Things
    
    * Fix Accessibility
    
    * Added control testing for finish and info
    
    * Added testing for control on info and stats. Also updated storage to contain new default sound
    
    * Store Updated Lengths
    
    * changed settings' close button name and classes for timer mode
    
    * partial tests for control and timer up
    
    * Working on stat display testing
    
    * Added more stats testing with local storage and timer to control-finish.js
    
    * Updated test-control-info/finish
    
    * add accessibility
    
    * add accessibility
    
    * Disable Cypress Screenshots
    
    * Disable Only on Work
    
    * Update Default Source
    
    * changed timer speed and minor settings css
    
    * Added test to info and stats to make sure button is disabled after setting is opened
    
    * Settings Enable Behaviour
    
    * Fix Timer Speeds
    
    * Re-Organize Control + Components
    
    * Merge Control
    
    * Fix Pomo-Storage Default
    
    * Lint Fix
    
    * Update test-control-finish.js
    
    Co-authored-by: Richard Duong <riduong@ucsd.edu>
    Co-authored-by: ntrappe <ntrappe@ucsd.edu>
    Co-authored-by: zhang <zhangfutian1@gmail.com>
    Co-authored-by: brandonsliu <bliu1002001@gmail.com>

---
+++ testing/cypress/test-control-info.js
@@ -0,0 +1,80 @@
+describe('Open Page', () => {
+  it('Opens index.html', () => {
+    cy.visit('./source/index.html');
+  });
+});
+
+describe('Testing Settings with Info', { includeShadowDom: true }, () => {
+  beforeEach(() => {
+    cy.visit('./source/index.html');
+  });
+
+  it('Toggling Dark Mode off & Check that css was changed', () => {
+    cy.get('#settings-button').click();
+    cy.get('#dark-slider').click();
+    cy.get('#settings-close-button').click();
+    cy.get('#info-styles').should(
+      'have.attr',
+      'href',
+      './components/pomo-info/pomo-info-light.css'
+    );
+  });
+
+  it('Toggling Dark Mode on & Check that css was changed', () => {
+    cy.get('#settings-button').click();
+    cy.get('#dark-slider').click();
+    cy.get('#dark-slider').click();
+    cy.get('#settings-close-button').click();
+    cy.get('#info-styles').should('have.attr', 'href', './components/pomo-info/pomo-info.css');
+  });
+
+  it('Opening Settings and Checking that info is not opened', () => {
+    cy.get('#settings-button').click();
+    cy.get('#info-button').should('be.disabled');
+  });
+});
+
+describe('Testing Timer with Info', { includeShadowDom: true }, () => {
+  beforeEach(() => {
+    cy.visit('./source/index.html');
+  });
+
+  it('Starting Timer and Checking that info is not opened', () => {
+    cy.get('#timer-button').click();
+    cy.get('#info-button').should('be.disabled');
+  });
+
+  it('Starting Timer, Opening and closing settings, and checking that info is disabled', () => {
+    cy.get('#timer-button').click();
+    cy.get('#settings-button').click();
+    cy.get('#settings-close-button').click();
+    cy.get('#info-button').should('be.disabled');
+  });
+
+  it('Starting Timer and stopping timer and Checking that info is opened', () => {
+    cy.get('#timer-button').click();
+    cy.get('#timer-button').click();
+    cy.get('#info-button').should('not.be.disabled');
+    cy.get('#info-button').click();
+    cy.get('#info-modal').should('have.css', 'display', 'block');
+  });
+});
+
+describe('Testing Stats with Info', { includeShadowDom: true }, () => {
+  beforeEach(() => {
+    cy.visit('./source/index.html');
+  });
+
+  it('Toggling Dark Mode on & Check that css was changed', () => {
+    cy.get('#finish-button').click();
+    cy.get('#info-button').should('be.disabled');
+  });
+
+  it('Open and close stats page and then open info page', () => {
+    cy.get('#finish-button').click();
+    cy.get('#statistics-close-button').click();
+    cy.get('#info-button').should('not.be.disabled');
+    cy.get('#info-button').click();
+    cy.get('#info-modal').should('have.css', 'display', 'block');
+  });
+});
