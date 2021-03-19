4e8a041cdd8aa779d3c5786cdc6c15d77484521a
Steven Steiner <steets250@gmail.com>
2021-03-18 18:58:10 -0700

    Merge Control

--- source/control/main.js
+++ source/control/main.js
@@ -2,14 +2,66 @@
  * @module Main
  */
 
-import './resize.js';
+import PomoAudio from '../components/pomo-audio.js';
+import PomoFinish from '../components/pomo-finish/pomo-finish.js';
+import PomoInfo from '../components/pomo-info/pomo-info.js';
+import PomoSettings from '../components/pomo-settings/pomo-settings.js';
+import * as PomoStorage from '../components/pomo-storage.js';
+import * as PomoTab from '../components/pomo-tab.js';
+import PomoTimer from '../components/pomo-timer/pomo-timer.js';
 
 /**
- * After init.js has been run by the page, retrieve components from window
+ * Initialize page styling
  */
-const { pomoAudio, pomoFinish, pomoInfo, pomoSettings, pomoTimer } = window;
-const PomoStorage = window.pomoStorage;
-const PomoTab = window.pomoTab;
+const stylesheet = document.createElement('link');
+stylesheet.rel = 'stylesheet';
+stylesheet.href = './index.css';
+stylesheet.setAttribute('href', './index.css');
+document.head.appendChild(stylesheet);
+
+/**
+ * Initialize web components
+ * Attach to window for method access and Cypress testing
+ */
+const pomoAudio = new PomoAudio();
+pomoAudio.setAttribute('id', 'pomo-audio');
+window.pomoAudio = pomoAudio;
+
+const pomoFinish = new PomoFinish();
+pomoFinish.setAttribute('id', 'pomo-finish');
+window.pomoFinish = pomoFinish;
+
+const pomoInfo = new PomoInfo();
+pomoInfo.setAttribute('id', 'pomo-info');
+window.pomoInfo = pomoInfo;
+
+const pomoSettings = new PomoSettings();
+pomoSettings.setAttribute('id', 'pomo-settings');
+window.pomoSettings = pomoSettings;
+
+window.pomoStorage = PomoStorage;
+
+window.pomoTab = PomoTab;
+
+const pomoTimer = new PomoTimer();
+pomoTimer.setAttribute('id', 'pomo-timer');
+window.pomoTimer = pomoTimer;
+
+/**
+ * Add components to their proper locations on the page
+ */
+const body = document.getElementById('body');
+body.appendChild(pomoAudio);
+
+const header = document.getElementById('header');
+header.appendChild(pomoSettings);
+header.appendChild(pomoInfo);
+
+const main = document.getElementById('main');
+main.appendChild(pomoTimer);
+
+const footer = document.getElementById('footer');
+footer.appendChild(pomoFinish);
 
 /**
  * Use Mode as an enum to prevent reuse of strings
